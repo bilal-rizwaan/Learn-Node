6ce601b15a12ff78fa2eba365745fef454aeb205
Bilal <bilal.rizwaan1909@gmail.com>
2021-04-14 00:16:01 +0500

    Day 2 In Node Js

--- Day_2.js
+++ Day_2.js
@@ -1,4 +1,12 @@
 const fs = require("fs");
-fs.writeFileSync("read.txt","There Is Some Node Stuff");
-fs.readFileSync("read.txt");
+// 1...  fs.writeFileSync("read.txt","There Is Some Node Stuff");
+// fs.readFileSync("read.txt");
 
+// 2... const bu= fs.readFileSync("read.txt");
+// console.log(bu);
+
+// 3... const bu= fs.readFileSync("read.txt");
+// const sh= bu.toString();
+// console.log(sh);
+
+fs.appendFileSync("read.txt","More Stuff Add");
